4e0b1b7473af829263c4047985361adcdaa7e1b6
Akanksha Kumari <200301120008@cutm.ac.in>
2024-08-11 17:56:06 +0530

    Loop questions added

---
+++ Loops/ForLoops.js
@@ -0,0 +1,5 @@
+//Loops are used to execute a piece of code again and again 
+for(i=1; i<=1000; i++){
+    console.log(" Love Yourself");
+}
+console.log("loop has ended");
---
+++ Practice3.js
@@ -0,0 +1,7 @@
+//calculate sum of 1 to 5 
+let sum = 0;
+for(let i=0; i<= 5; i++){
+    sum = sum+i
+}
+console.log("sum = ", sum);
+console
