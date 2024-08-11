db399ac6a13b0a1a76c1eaa87306c9280d984f99
Akanksha Kumari <200301120008@cutm.ac.in>
2024-08-11 23:29:59 +0530

    file added

---
+++ Array/LoopingOverArray.js
@@ -0,0 +1,23 @@
+//print all the elements of an array 
+
+// let marks = [98 , 67 ,54 , 90, 23, 89, 200];
+
+// /*for (let i = 0; i < marks.length; i++){
+//     console.log(marks[i]);
+// }*/
+
+
+// //for of 
+
+// for(let mark of marks){
+//     console.log(marks);
+// }
+
+
+
+let cities = ["delhi", "pune", "mumbai", "hyderabad", "gurgaon"];
+
+for(let city of cities){
+    console.log(city);
+    console.log(city.toUpperCase());
+}
---
+++ Array/StudentsMarks.js
@@ -0,0 +1,7 @@
+let marks = [97, 82, 75, 64, 36];
+console.log(marks);
+console.log(marks.length);
+
+let heroes = ["ironman", "thor", "hulk", "shaktiman", "spiderman", "antman"];
+console.log(heroes);
+
