39c29e66759fc31c99cd2f5b712aa03583672690
Akanksha Kumari <200301120008@cutm.ac.in>
2024-08-12 15:40:14 +0530

    practice question added

---
+++ PracticeQuestions/Practice9.js
@@ -0,0 +1,19 @@
+/*Qs. Create a function using the "function" keyword that takes a String as an argument &
+returns the number of vowels in the string.
+*/
+
+function countVowels(str) {
+    let count = 0;
+    for(const char of str){
+        if(
+            char === "a" ||
+            char === "e" ||
+            char === "i" || 
+            char === "o" || 
+            char === "u"
+        ) {
+            count++;
+        }
+    }
+    console.log(count);
+}
